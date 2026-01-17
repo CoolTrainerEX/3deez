84468d0e1f05bbbdd2b6c3111a747cd1105e0b34
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-01-17 20:52:27 +0800

    what

--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,46 @@
-import "./style.css";
-import cat from "./cat.webp";
-import { Euler, PerspectiveCamera, Quaternion, Vector3 } from "three";
-import { degreesToRadians } from "./util.ts";
+import {
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  SphereGeometry,
+  Vector3,
+  WebGLRenderer,
+} from "three";
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const width = canvas.width = innerWidth,
-  height = canvas.height = innerHeight,
-  speed = 1e-3;
-const ctx = canvas.getContext("2d")!;
-
-const camera = new PerspectiveCamera(undefined, width / height);
-const points = Array.from(
-  { length: 100 },
-  () => (new Vector3()).random().subScalar(0.5).multiplyScalar(10),
+const scene = new Scene();
+const camera = new PerspectiveCamera(undefined, innerWidth / innerHeight);
+const renderer = new WebGLRenderer(
+  /*{
+  canvas: document.getElementById("c") as HTMLCanvasElement,
+}*/
 );
 
-const img = new Image();
+renderer.setSize(innerWidth, innerHeight);
 
-img.src = cat;
+const starMesh = new Mesh(
+  new SphereGeometry(0.01),
+  new MeshBasicMaterial({ color: Color.NAMES.white }),
+);
 
-function draw() {
-  ctx.clearRect(0, 0, width, height);
+const stars = Array.from({ length: 100 }, () => {
+  const star = starMesh.clone();
+  star.position.copy((new Vector3()).random().subScalar(0.5));
 
-  for (const point of points.map((value) => value.clone().project(camera))) {
-    // Check if valid render or invalid matrix calculation.
-    if (point.z >= -1 && point.z <= 1) {
-      ctx.drawImage(
-        img,
-        (point.x + 1) / 2 * width,
-        (-point.y + 1) / 2 * height,
-        100,
-        100,
-      );
-    }
-  }
-}
-
-requestAnimationFrame(draw);
-
-addEventListener("mousemove", (ev) => {
-  camera.rotateX(ev.movementY * -speed);
-  camera.rotateY(ev.movementX * -speed);
-  camera.updateMatrixWorld();
-  requestAnimationFrame(draw);
+  return star;
 });
 
-const deviceEuler = new Euler();
-const deviceQuaternion = new Quaternion();
-const screenTransform = new Quaternion();
-const worldTransform = new Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5));
+for (const star of stars) {
+  scene.add(star);
+}
+camera.position.z = 5;
 
-addEventListener("deviceorientation", (ev) => {
-  deviceEuler.set(
-    degreesToRadians(ev.beta ?? 0),
-    degreesToRadians(ev.alpha ?? 0),
-    -degreesToRadians(ev.gamma ?? 0),
-    "YXZ",
-  );
+renderer.setAnimationLoop(() => {
+  const canvas = renderer.domElement;
 
-  deviceQuaternion.setFromEuler(deviceEuler);
-
-  screenTransform.setFromAxisAngle(
-    new Vector3(0, 0, 1),
-    -degreesToRadians(screen.orientation.angle ?? 0),
-  );
-
-  deviceQuaternion.multiply(screenTransform);
-  deviceQuaternion.multiply(worldTransform);
-  camera.quaternion.copy(deviceQuaternion);
-  camera.updateMatrixWorld();
-  requestAnimationFrame(draw);
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+  camera.translateZ(-0.001);
+  renderer.render(scene, camera);
 });
