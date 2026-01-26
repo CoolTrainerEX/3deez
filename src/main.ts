92cdec8b6ba48c7c35a7e91a92474e9821f465eb
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-01-26 17:40:55 +0800

    card wiggies

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import cat from "./cat.webp";
 import { Euler, PerspectiveCamera, Quaternion, Vector3 } from "three";
 import { degreesToRadians } from "./util.ts";
 
-const speed = 0.5, size = 1000, sensitivity = 0.0005;
+const speed = 0.1, size = 1000, sensitivity = 0.0005;
 const canvas = document.querySelector("canvas")!;
 const ctx = canvas.getContext("2d")!;
 
 const camera = new PerspectiveCamera();
-const destQuaternion = new Quaternion();
+const destRotation = new Quaternion();
 const points = Array.from(
   { length: 100 },
   () => new Vector3().random().subScalar(0.5).multiplyScalar(10),
@@ -32,7 +32,7 @@ function draw() {
   }
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  camera.quaternion.slerp(destQuaternion, speed);
+  camera.quaternion.slerp(destRotation, speed);
   camera.updateMatrixWorld();
 
   for (const point of points.map((value) => value.clone().project(camera))) {
@@ -54,7 +54,7 @@ function draw() {
 requestAnimationFrame(draw);
 
 addEventListener("mousemove", (ev) => {
-  destQuaternion.multiply(new Quaternion().setFromEuler(
+  destRotation.multiply(new Quaternion().setFromEuler(
     new Euler().set(
       -ev.movementY * sensitivity,
       -ev.movementX * sensitivity,
@@ -75,7 +75,7 @@ const worldTransform = new Quaternion().setFromAxisAngle(
 );
 
 addEventListener("deviceorientation", (ev) => {
-  destQuaternion.setFromEuler(
+  destRotation.setFromEuler(
     new Euler(
       degreesToRadians(ev.beta ?? 0),
       degreesToRadians(ev.alpha ?? 0),
@@ -84,14 +84,21 @@ addEventListener("deviceorientation", (ev) => {
     ),
   );
 
-  destQuaternion.multiply(worldTransform);
+  destRotation.multiply(worldTransform);
 });
 
 const card = document.querySelector(".card") as HTMLDivElement;
 
-card.addEventListener("mousemove", (ev) => {
-  card.style.transform = `rotateY(${
-    (ev.offsetX / card.clientWidth * 2 -
-      1) * 0.5
+function rotateCard() {
+  const delta = new Euler().setFromQuaternion(
+    destRotation.clone().invert().multiply(camera.quaternion),
+    "YXZ",
+  );
+
+  card.style.transform = ` rotateX(${delta.x * Math.PI}rad) rotateY(${
+    -delta.y * Math.PI
   }rad)`;
-});
+  requestAnimationFrame(rotateCard);
+}
+
+requestAnimationFrame(rotateCard);
